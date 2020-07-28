418f244d464cc08cd49a05da3203454097c98239
tristo999 <tristanfitzwater@gmail.com>
2020-07-28 19:19:19 -0400

    Inital Commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Home() {
+  return <div style={{ color: `purple`, fontSize: `72px` }}>
+<h1>Wassup Dog!</h1>
+<p>I need to learn how this works...</p>
+  </div>
+}
