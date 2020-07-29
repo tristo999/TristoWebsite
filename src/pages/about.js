698a03ccf025ef348acc0c8e6894f480258adba5
tristo999 <tristanfitzwater@gmail.com>
2020-07-28 22:14:00 -0400

    Gatsby Tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header.js"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header tommyText="This is my about Header" />
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>
-<h1>Wassup Dog!</h1>
-<p>I need to learn how this works...</p>
-  </div>
+  return (
+    <div style={{ color: `purple`, fontSize: `46px` }}>
+      <Header tommyText="This is my Index Header" />
+      <h1>Wassup Dog!</h1>
+      <Link to="/about/">About</Link>
+      <br />
+      <Link to="/contact/">Contact</Link>
+      <br />
+      <Link to="/missingPage/">Link that doesn't work</Link>
+      <br />
+      <Link to="https://www.google.com">Google Link</Link>
+      <p>I need to learn how this works...</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
