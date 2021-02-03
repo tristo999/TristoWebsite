00968db67f0d2ee9256919960d40aecab6aba47a
tristo999 <tristanfitzwater@gmail.com>
2021-02-03 14:53:51 -0500

    Updates

---
+++ component---src-pages-404-js-801796087b06bba6a63f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,t,e){"use strict";e.r(t);var r=e("dI71"),o=e("q1tI"),u=e.n(o),a=function(n){function t(){return n.apply(this,arguments)||this}return Object(r.a)(t,n),t.prototype.render=function(){return u.a.createElement("h1",null,"Sad 404 is sad.")},t}(o.Component);t.default=a}}]);
+//# sourceMappingURL=component---src-pages-404-js-801796087b06bba6a63f.js.map
